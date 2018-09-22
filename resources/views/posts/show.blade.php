052c9b4aaac291311a528eaadbe9073c9f3d3c9e
B.A. Imambaks Rodrigues <0924777@hr.nl>
2018-09-22 23:41:03 +0200

    added all crud functionality

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,4 +12,10 @@
                 {{$post->body}}
         </div>
     </div>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit post</a>
+
+    {{Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])}}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {{Form::close()}}
     @endsection
